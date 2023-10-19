81c098df50849f5c4e67b258dbda2c2b98e5accd
ntdien <ntdien@tma.com.vn>
2023-10-19 15:15:35 +0700

    init simple video player

--- lms-ui/src/app/modules/courses/lesson-detail/course-lesson-detail.component.ts
+++ lms-ui/src/app/modules/courses/lesson-detail/course-lesson-detail.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { VideoPlayerComponent } from '@shared/components/video-player/video-player.component';
+import { FooterComponent } from '@shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-course-lesson-detail',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, VideoPlayerComponent],
   templateUrl: './course-lesson-detail.component.html',
   styleUrls: ['./course-lesson-detail.component.scss']
 })
